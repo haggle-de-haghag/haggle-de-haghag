6a721221d4b2be88a68dffb5d07377b7db8fe1a6
Osamu Koga <osak.63@gmail.com>
2021-05-03 03:40:05 -0500

    Add production URL to CORS config

--- src/main/java/jp/osak/haggledehaghag/config/CorsConfig.kt
+++ src/main/java/jp/osak/haggledehaghag/config/CorsConfig.kt
@@ -5,10 +5,10 @@ import org.springframework.web.servlet.config.annotation.CorsRegistry
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
 @Configuration
-open class LocalCorsConfig : WebMvcConfigurer {
+open class CorsConfig : WebMvcConfigurer {
     override fun addCorsMappings(registry: CorsRegistry) {
         registry.addMapping("/**")
             .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE")
-            .allowedOrigins("http://localhost:3000")
+            .allowedOrigins("http://localhost:3000", "https://haggle-de-haghag.osak.jp")
     }
 }
