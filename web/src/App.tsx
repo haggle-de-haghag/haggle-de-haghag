ffe85ee0349f88469f70d7c31e7439f5deb08474
Osamu Koga <osak.63@gmail.com>
2021-05-23 05:44:27 -0500

    Add menu bar

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,9 +1,14 @@
 import React from 'react';
-import {Box, Container} from "@material-ui/core/index";
+import {AppBar, Box, Container, Link, Toolbar, Typography} from "@material-ui/core/index";
 
 export default function App(props: any) {
   return (
       <Container>
+          <AppBar position="static" color="transparent" elevation={1}>
+              <Toolbar>
+                  <Typography variant="h6"><Link href="/" color="inherit">ハグルでハグハグ</Link></Typography>
+              </Toolbar>
+          </AppBar>
           <Box my={4}>
               {props.children}
           </Box>
