28767402e5f32f57c7cab88935f871694d1d07fd
Osamu Koga <osak.63@gmail.com>
2021-06-19 05:52:10 -0500

    Add token count changer UI #11

--- web/src/component/GameSummaryPane.tsx
+++ web/src/component/GameSummaryPane.tsx
@@ -1,7 +1,8 @@
 import {Player, Rule, RuleAccessMap, Token, TokenAllocationMap} from "../model";
 import {PlayerIdWithAccess} from "../rest/gameMaster";
 import {ListItem, Table, TableBody, TableCell, TableHead, TableRow} from "@material-ui/core";
-import {Avatar, Box, List, makeStyles} from "@material-ui/core/index";
+import {Avatar, Box, Button, Grid, IconButton, List, makeStyles} from "@material-ui/core/index";
+import {AddCircle, RemoveCircle} from "@material-ui/icons";
 
 interface Props {
     players: Player[];
@@ -12,6 +13,10 @@ interface Props {
 }
 
 const useStyles = makeStyles((theme) => ({
+    ruleListBox: {
+        display: 'flex',
+        flexWrap: 'wrap',
+    },
     assignedRule: {
         backgroundColor: theme.palette.primary.main,
     },
@@ -19,7 +24,18 @@ const useStyles = makeStyles((theme) => ({
         backgroundColor: theme.palette.secondary.main,
     },
     unknownRule: {
-    }
+    },
+    allocationListBox: {
+        display: 'flex',
+        justifyContent: 'space-between',
+        flexWrap: 'nowrap',
+        whiteSpace: 'nowrap',
+    },
+    allocationButtonList: {
+        display: 'flex',
+        justifyContent: 'flex-end',
+        flexWrap: 'nowrap',
+    },
 }));
 
 export default function GameSummaryPane(props: Props) {
@@ -29,7 +45,7 @@ export default function GameSummaryPane(props: Props) {
         return <TableRow key={player.id}>
             <TableCell>{player.displayName}</TableCell>
             <TableCell>
-                <Box style={{display: 'flex'}}>
+                <Box className={styles.ruleListBox}>
                     {props.rules.map((r) => {
                         const accessList = props.ruleAccessList[r.id];
                         const access = accessList?.find((a) => a.playerId == player.id);
@@ -48,7 +64,15 @@ export default function GameSummaryPane(props: Props) {
                     {props.tokens.map((t) => {
                         const allocationList = props.tokenAllocationMap[t.id];
                         const allocation = allocationList?.find((a) => a.playerId == player.id);
-                        return allocation && <ListItem key={t.id}>{t.title}: {allocation.amount}</ListItem>;
+                        return <ListItem key={t.id}>
+                            <Grid container className={styles.allocationListBox} spacing={4}>
+                                <Grid item>{t.title}: {allocation?.amount ?? 0}</Grid>
+                                <Grid item container className={styles.allocationButtonList}>
+                                    <Grid item><IconButton size="small"><AddCircle /></IconButton></Grid>
+                                    <Grid item><IconButton size="small"><RemoveCircle /></IconButton></Grid>
+                                </Grid>
+                            </Grid>
+                        </ListItem>;
                     })}
                 </List>
             </TableCell>
