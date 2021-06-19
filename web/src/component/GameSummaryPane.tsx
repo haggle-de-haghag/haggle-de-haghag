7c774e4e4b1a6a86085c8f8b2f4a1bc730705ee5
Osamu Koga <osak.63@gmail.com>
2021-06-19 02:06:30 -0500

    Show tokens and player key (#12)

--- web/src/component/GameSummaryPane.tsx
+++ web/src/component/GameSummaryPane.tsx
@@ -1,7 +1,7 @@
 import {Player, Rule, RuleAccessMap, Token, TokenAllocationMap} from "../model";
 import {PlayerIdWithAccess} from "../rest/gameMaster";
-import {Table, TableBody, TableCell, TableHead, TableRow} from "@material-ui/core";
-import {Avatar, makeStyles} from "@material-ui/core/index";
+import {ListItem, Table, TableBody, TableCell, TableHead, TableRow} from "@material-ui/core";
+import {Avatar, Box, List, makeStyles} from "@material-ui/core/index";
 
 interface Props {
     players: Player[];
@@ -28,18 +28,31 @@ export default function GameSummaryPane(props: Props) {
     const renderPlayerRow = (player: Player) => {
         return <TableRow key={player.id}>
             <TableCell>{player.displayName}</TableCell>
-            <TableCell style={{ display: 'flex' }}>
-                {props.rules.map((r) => {
-                const accessList = props.ruleAccessList[r.id];
-                const access = accessList?.find((a) => a.playerId == player.id);
-                const className = access == null
-                    ? styles.unknownRule
-                    : access.accessType == 'ASSIGNED'
-                        ? styles.assignedRule
-                        : styles.sharedRule;
+            <TableCell>
+                <Box style={{display: 'flex'}}>
+                    {props.rules.map((r) => {
+                        const accessList = props.ruleAccessList[r.id];
+                        const access = accessList?.find((a) => a.playerId == player.id);
+                        const className = access == null
+                            ? styles.unknownRule
+                            : access.accessType == 'ASSIGNED'
+                                ? styles.assignedRule
+                                : styles.sharedRule;
 
-                return <Avatar className={className} key={r.id}>{r.ruleNumber}</Avatar>;
-            })}</TableCell>
+                        return <Avatar className={className} key={r.id}>{r.ruleNumber}</Avatar>;
+                    })}
+                </Box>
+            </TableCell>
+            <TableCell>
+                <List>
+                    {props.tokens.map((t) => {
+                        const allocationList = props.tokenAllocationMap[t.id];
+                        const allocation = allocationList?.find((a) => a.playerId == player.id);
+                        return allocation && <ListItem key={t.id}>{t.title}: {allocation.amount}</ListItem>;
+                    })}
+                </List>
+            </TableCell>
+            <TableCell>{player.playerKey}</TableCell>
         </TableRow>
     };
 
@@ -48,6 +61,8 @@ export default function GameSummaryPane(props: Props) {
             <TableRow>
                 <TableCell>プレイヤー</TableCell>
                 <TableCell>ルール所持状況</TableCell>
+                <TableCell>トークン所持状況</TableCell>
+                <TableCell>プレイヤーキー</TableCell>
             </TableRow>
         </TableHead>
         <TableBody>
