0aa455b665686655a9405f2de3280fe6dd7d9d00
Osamu Koga <osak.63@gmail.com>
2021-07-24 15:31:59 -0500

    Update iframe height after the all contents are loaded (#25)

--- web/src/component/IFrameView.tsx
+++ web/src/component/IFrameView.tsx
@@ -16,19 +16,29 @@ export function IFrameView(props: Props) {
     const ref = useRef(null as (HTMLIFrameElement | null));
     const styles = useStyle();
 
+    const setHeight = () => {
+        const current = ref.current;
+        if (current) {
+            const doc = current.contentDocument;
+            if (doc) {
+                current.height = `${doc.scrollingElement?.scrollHeight ?? 0}px`;
+            }
+        }
+    };
+
     useEffect(() => {
         const current = ref.current;
         if (current) {
-            const doc = ref.current?.contentDocument;
+            const doc = current.contentDocument;
             if (doc) {
                 current.height = '0';
                 doc.open();
                 doc.write(props.html.replaceAll("\n", "<br>"));
                 doc.close();
-                current.height = `${doc.scrollingElement?.scrollHeight ?? 0}px`;
+                setHeight();
             }
         }
     }, [props.html, ref.current]);
 
-    return <iframe ref={ref} className={styles.iframe} />
+    return <iframe ref={ref} className={styles.iframe} onLoad={setHeight}/>
 }
