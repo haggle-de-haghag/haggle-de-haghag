a4f492c325b98d06af2f82ed257eebe2c54ec86e
Osamu Koga <osak.63@gmail.com>
2021-08-06 04:34:44 -0500

    dnd improved

--- web-angular/src/app/gm-page/item-list/item-list.component.ts
+++ web-angular/src/app/gm-page/item-list/item-list.component.ts
@@ -1,7 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {DBService} from "../db.service";
 import {UIService} from "../ui.service";
-import {MatSelectionListChange} from "@angular/material/list";
 import {Rule, Token} from "../../model";
 
 @Component({
@@ -18,24 +17,14 @@ export class ItemListComponent implements OnInit {
 
   get rules() { return this.dbService.rules; }
   get tokens() { return this.dbService.tokens; }
+  get selectedRule() { return this.uiService.selectedRule; }
+  get selectedToken() { return this.uiService.selectedToken; }
 
-  select(event: MatSelectionListChange) {
-    if (event.options.length == 0) {
-      return;
-    }
+  selectRule(rule: Rule) {
+    this.uiService.selectRule(rule);
+  }
 
-    const option = (event.options[0].value as OptionValue);
-    switch (option.type) {
-      case 'rule':
-        this.uiService.selectRule(option.rule);
-        break;
-      case 'token':
-        this.uiService.selectToken(option.token);
-        break;
-    }
+  selectToken(token: Token) {
+    this.uiService.selectToken(token);
   }
 }
-
-export type OptionValue =
-      { type: 'rule', rule: Rule }
-    | { type: 'token', token: Token };
